132980bc58c717c9788a57e25eb235581464b942
Gatounet <starz.gatounet@gmail.com>
2026-07-19 21:19:21 +0200

    Intègre Firebase Messaging pour Android

---
+++ app/lib/src/core/notifications/firebase_messaging_service.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+class FirebaseMessagingService {
+  const FirebaseMessagingService._();
+
+  static Future<void> initialize() async {
+    if (kIsWeb || defaultTargetPlatform != TargetPlatform.android) return;
+    await Firebase.initializeApp();
+    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+    final messaging = FirebaseMessaging.instance;
+    await messaging.requestPermission(alert: true, badge: true, sound: true);
+    await messaging.getToken();
+    FirebaseMessaging.onMessage.listen((message) {
+      debugPrint('HomeQuest notification: ${message.notification?.title}');
+    });
+  }
+}
